34ba12df781608783d88a60b6386b822c0bab703
Ethan Spencer <ethan.spencer@zenenergy.com.au>
2025-06-04 15:44:56 +1000

    feat: evironment agnostic spark session init

---
+++ src/spark_init.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from databricks.connect import DatabricksSession
+
+_spark_session = None
+
+def get_spark_session():
+    global _spark_session
+    if _spark_session == None:
+        
+        try:
+            _spark_session = SparkSession.builder.getOrCreate()
+        except:
+            try:
+                _spark_session = DatabricksSession.builder.getOrCreate()
+            except Exception as error:
+                raise RuntimeError("Failed to initialize spark session.") from error
+    return _spark_session
+
